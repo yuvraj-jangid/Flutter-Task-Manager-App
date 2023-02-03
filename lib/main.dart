c2730e7a220b1ee7b90de2f9b328c5dd301af1a4
Xenomorphing79 <gourabb52@gmail.com>
2023-02-03 18:57:58 +0530

    Connected Firebase to the Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,15 +11,37 @@ void main() async {
   runApp(const MyApp());
 }
 
+// ignore: must_be_immutable
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
+  static firebase_auth.FirebaseAuth firebaseAuth =
+      firebase_auth.FirebaseAuth.instance;
+  void signup() async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: "test@gmail.com", password: "123456");
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Firebase"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              signup();
+            },
+            child: const Text("Sign up"),
+          ),
         ),
       ),
     );
